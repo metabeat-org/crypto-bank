548ea5f6403eee341bc2757811c90e58d5e2e6d6
dajungyang <djyang@tnetkorea.com>
2024-08-05 13:34:54 +0900

    client: deposit 안쓰는 컴포넌트 삭제

--- client/src/app/(main)/deposit/page.tsx
+++ client/src/app/(main)/deposit/page.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { NextPage } from "next";
-import { Steps } from "@/components/pages/deposit/steps";
-import { ProgressByStep } from "@/components/pages/deposit/progress-by-step";
-import { StepActionButtons } from "@/components/pages/deposit/step-action-buttons";
 
-const CreatePage: NextPage = () => {
+const DepositPage: NextPage = () => {
     return (
         <div className="flex flex-col">
-            <Steps />
-            <ProgressByStep />
-            <StepActionButtons />
+
         </div>
     );
 };
 
-export default CreatePage;
+export default DepositPage;
