11a1fc8d6a3fe9415c1ab80c2eee3dc8e3599d80
dajungyang <djyang@tnetkorea.com>
2024-08-05 15:12:42 +0900

    client: deposit page test commit

--- client/src/app/(main)/deposit/page.tsx
+++ client/src/app/(main)/deposit/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NextPage } from "next";
 
 const DepositPage: NextPage = () => {
-    return <div className="flex flex-col">deposit page test</div>;
+    return <div className="flex flex-col">deposit page</div>;
 };
 
 export default DepositPage;
