af4ab880b1ffd49a6d2c264794c5acaea9f13e50
dajungyang <djyang@tnetkorea.com>
2024-08-07 11:29:08 +0900

    withdrawal 페이지 초기화

--- client/src/app/(main)/withdrawal/page.tsx
+++ client/src/app/(main)/withdrawal/page.tsx
@@ -1,71 +1,38 @@
+"use client";
+
 import React from "react";
 import { NextPage } from "next";
 
 const WithdrawalPage: NextPage = () => {
     return (
         <div className="flex flex-col">
-            <div>
+            <div className="flex items-center">
+                <h2 className="text-3xl font-bold">Withdrawal History</h2>
                 <button
-                    className="btn"
-                    // onClick={() =>
-                    //     document.getElementById("my_modal_1").showModal()
-                    // }
+                    className="btn btn-primary ml-auto"
+                    onClick={() =>
+                        document.getElementById("my_modal_1").showModal()
+                    }
                 >
-                    open modal
+                    Withdrawal
                 </button>
-                <dialog
-                    id="my_modal_1"
-                    className="modal"
-                >
-                    <div className="modal-box">
-                        <h3 className="font-bold text-lg">Hello!</h3>
-                        <p className="py-4">
-                            Press ESC key or click the button below to close
-                        </p>
-                        <div className="modal-action">
-                            <form method="dialog">
-                                {/* if there is a button in form, it will close the modal */}
-                                <button className="btn">Close</button>
-                            </form>
-                        </div>
-                    </div>
-                </dialog>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr className="hover">
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
-                    </tbody>
-                </table>
+                {/*<dialog*/}
+                {/*    id="my_modal_1"*/}
+                {/*    className="modal"*/}
+                {/*>*/}
+                {/*    <div className="modal-box">*/}
+                {/*        <h3 className="font-bold text-lg">Hello!</h3>*/}
+                {/*        <p className="py-4">*/}
+                {/*            Press ESC key or click the button below to close*/}
+                {/*        </p>*/}
+                {/*        <div className="modal-action">*/}
+                {/*            <form method="dialog">*/}
+                {/*                /!* if there is a button in form, it will close the modal *!/*/}
+                {/*                <button className="btn">Close</button>*/}
+                {/*            </form>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</dialog>*/}
             </div>
         </div>
     );
