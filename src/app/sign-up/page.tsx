939b709413e8cd2349e41e771908b6f34b754bfa
dajungyang <djyang@tnetkorea.com>
2024-08-07 21:28:10 +0900

    sign-up api 추가

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,31 +1,88 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 import { DefaultInput } from "@/components/default-input";
+import { useAtom, useAtomValue } from "jotai/index";
+import { loadingAtom, walletAtom } from "@/stores";
+import { redirect } from "next/navigation";
 
 const SignUpPage: NextPage = () => {
+    const [isLoading, setLoading] = useAtom(loadingAtom);
+    const { account } = useAtomValue(walletAtom);
+    const [form, setForm] = useState<{ email: string; userName: string }>({
+        email: "",
+        userName: "",
+    });
+    const handleSignUp = async e => {
+        e.preventDefault();
+
+        try {
+            setLoading(true);
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/auth/sign-up`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ ...form, walletAddress: account }),
+                }
+            );
+            setLoading(false);
+
+            const { status } = await res.json();
+
+            if (status === 200) {
+                redirect("/home");
+            }
+        } catch (e) {
+            setLoading(false);
+            console.error("Failed to sign up", e);
+        }
+    };
+
     return (
         <div className="card flex flex-col items-center">
             <div className="card-body mt-20 w-96 bg-base-200 max-w-sm rounded-xl">
                 <h2 className="mb-4 font-bold text-2xl text-center text-warning">
                     Sign Up
                 </h2>
-                <form>
+                <form onSubmit={handleSignUp}>
                     <div className="flex flex-col">
                         <DefaultInput
                             label="Wallet Address"
                             inputProps={{
-                                value: "0xAcCb238DE153b1Ea095134CFC79D2b80F7BD0D79",
+                                defaultValue: account,
                                 readOnly: true,
                             }}
                         />
-                        <DefaultInput label="Email" />
-                        <DefaultInput label="User Name" />
+                        <DefaultInput
+                            label="Email"
+                            inputProps={{
+                                value: form.email,
+                                onChange: e =>
+                                    setForm(form => ({
+                                        ...form,
+                                        email: e.target.value,
+                                    })),
+                            }}
+                        />
+                        <DefaultInput
+                            label="User Name"
+                            inputProps={{
+                                value: form.userName,
+                                onChange: e =>
+                                    setForm(form => ({
+                                        ...form,
+                                        userName: e.target.value,
+                                    })),
+                            }}
+                        />
                     </div>
                     <button
                         className="mt-6 btn btn-neutral w-full"
-                        disabled
+                        disabled={isLoading}
                     >
                         Sign Up
                     </button>
